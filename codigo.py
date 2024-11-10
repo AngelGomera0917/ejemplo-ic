a63b404da5433cd3304b09b1301b5701ccb9fda5
Angel Antonio Gomera <antoniogomera12@gmail.com>
2024-11-10 10:27:09 -0600

    Mi primer commit

---
+++ codigo.py
@@ -0,0 +1,9 @@
+# suma.py
+
+def sumar(a, b):
+    return a + b
+
+# Usamos la función para sumar 2 + 2
+if __name__ == "__main__":
+    resultado = sumar(2, 2)
+    print(f"El resultado de 2 + 2 es {resultado}")
---
+++ test/test_codigo.py
@@ -0,0 +1,14 @@
+# test_sumar.py
+import unittest
+from codigo import sumar
+
+class TestSumar(unittest.TestCase):
+
+    def test_sumar(self):
+        # Probamos la función sumar con diferentes valores
+        self.assertEqual(sumar(2, 2), 4)  # Suma de 2 + 2 debe ser 4
+        self.assertEqual(sumar(0, 0), 0)  # Suma de 0 + 0 debe ser 0
+        self.assertEqual(sumar(-1, 1), 0)  # Suma de -1 + 1 debe ser 0
+
+if __name__ == '__main__':
+    unittest.main()
